ed5915b20868c68038f40570c10d8cbe61753274
Shubham-Bhosale7 <shubhambhosale2299@gmail.com>
2026-02-22 16:39:48 +0530

    Starting FoodExplorer

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-blue-200 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-white">React + Tailwind CRA Starter</h1>
+    </div>
+  );
+}
+
+export default App;
